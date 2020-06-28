b60a6d2f17fa4480cedebc8fcf4e687da6553f8e
DiracSpace <deleon.martinez.16138@itsmante.edu.mx>
2020-06-27 23:53:58 -0400

    Added header responsiveness & carousel for mobile

---
+++ js/base.js
@@ -0,0 +1,26 @@
+const emailForm = document.querySelector('#email-form');
+const modal = document.querySelector('#email');
+const personal = '6861636b363332343340676d61696c2e636f6d'
+emailForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    Email.send({
+        SecureToken: "abc1b022-6cad-4003-b71d-647ffe207d23",
+        To: translate(personal),
+        From: emailForm['email-from'].value,
+        Subject: emailForm['email-subject'].value,
+        Body: emailForm['email-body'].value,
+    }).then(
+        M.toast({ html: '¡Correo enviado!', classes: 'rounded' })
+    );
+    M.Modal.getInstance(modal).close();
+    emailForm.reset();
+});
+
+function translate(input) {
+    var stringinput = input.toString();
+    var output = '';
+    for (var n = 0; n < stringinput.length; n += 2) {
+        output += String.fromCharCode(parseInt(stringinput.substr(n, 2), 16));
+    }
+    return output;
+}
